07064ffb35fb6cc8ec17f6c394ab301bb290c412
SHUBHAKANT NAYAK <146442931+nayakshubhakant9@users.noreply.github.com>
2025-03-12 18:04:06 +0530

    loops

---
+++ CHAPTER 7/03_list_using_while.py
@@ -0,0 +1,6 @@
+l = [1, "Shubhakant", False, "This", "Rohan", "Vinay", "Kabita"]
+i = 0
+
+while(i<len(l)):
+    print(l[i])
+    i +=1
---
+++ CHAPTER 7/05_for_loops_iterate.py
@@ -0,0 +1,14 @@
+# For loop with lists
+l = [1, 4, 6, 234, 764]
+for i in l:
+    print(i)
+
+# for Loop with tuples
+t = (6, 231, 75, 122)
+for i in  t:
+    print(i)
+
+# for Loop with strings  
+s = "Shubhakant"
+for i in s:
+    print(i)
