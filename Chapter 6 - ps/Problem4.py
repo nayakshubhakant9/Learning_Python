2716a7499fbef475791e18e20e27742d3f4b8de7
Shubhakant Nayak <nayakshubhakant9@gmail.com>
2025-07-07 17:06:32 +0530

    Practice_set_6

--- Chapter 6 - ps/Problem4.py
+++ Chapter 6 - ps/Problem4.py
@@ -1,3 +1,5 @@
+# Write a program to find whether a given username contains less than 10
+#characters or not.
 
 username = input("Enter username: ")
 
--- Chapter 6 - ps/Problem7.py
+++ Chapter 6 - ps/Problem7.py
@@ -1,3 +1,5 @@
+#  Write a program to find out whether a given post is talking about “Harry” or not.
+
 post = input("Enter the post: ")
 
 if("shubhakant" in post.lower()):
